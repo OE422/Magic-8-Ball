f1e541216889a72ee65c0ae1d4969f253ccb6d14
Omar E <omareldoronki@icloud.com>
2019-09-15 19:05:54 -0400

    experimenting with a round button

--- Magic 8-Ball/ViewController.swift
+++ Magic 8-Ball/ViewController.swift
@@ -9,10 +9,20 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    var circleButton = UIButton()
+    var otherButton = UIButton()
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = UIColor.black
+        circleButton.backgroundColor = UIColor.white
+        
+        circleButton = UIButton(frame: CGRect(x: UIScreen.main.bounds.width/2, y: UIScreen.main.bounds.height/2, width: 30, height: 30))
+        circleButton.layer.cornerRadius = 200
+        circleButton.clipsToBounds = true
+        circleButton.setTitle("8", for: .normal)
+        self.view.addSubview(circleButton)
+        otherButton.backgroundColor = UIColor.white
         
     }
 
